dd52d31214d063b505c79eeb03857684e76e9d01
Sowjanya_2005614 <55553331+mannisowjanya@users.noreply.github.com>
2020-06-09 12:21:52 +0530

    Update arithmetic.c

--- arithmetic.c
+++ arithmetic.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main()
 {
-int a,b,sum,diff,mul,div,mod;
+int a,b,sum,diff,mul,division,mod;
 scanf("%d,%d",&a,&b);
 sum=a+b;
 diff=a-b;
 mul=a*b;
-div=a/b;
+division=a/b;
 mod=a%b;
-printf("sum \n%d\ndiff \n%d\nmul \n%d\ndiv \n%d\n mod \n%d",sum,diff,mul,div,mod);
+printf("sum \n%d\ndiff \n%d\nmul \n%d\ndiv \n%d\n mod \n%d",sum,diff,mul,division,mod);
 }
