e798e6b10ab3cee6c0211ecd8082f62a67e824ab
Sowjanya_2005614 <55553331+mannisowjanya@users.noreply.github.com>
2020-06-09 12:21:10 +0530

    Update arithmetic.c

--- arithmetic.c
+++ arithmetic.c
@@ -2,11 +2,11 @@
 int main()
 {
 int a,b,sum,diff,mul,div,mod;
-scanf("%d,%d",&a,&)b;
-sum=a_b;
+scanf("%d,%d",&a,&B);
+sum=a+b;
 diff=a-b;
 mul=a*b;
-div=d\b;
+div=a/b;
 mod=a%b;
-printf("sum \n%d\ndiff \n%d\nmul \n%d\ndiv \n%d\n mod \n%d",sum,diff,mul,div,md);
+printf("sum \n%d\ndiff \n%d\nmul \n%d\ndiv \n%d\n mod \n%d",sum,diff,mul,div,mod);
 }
